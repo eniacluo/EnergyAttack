f77303838a7928eadec537a395149db484632fd5
simon <eniacsimon@gmail.com>
2019-02-05 20:09:46 -0500

    combine to one single ssh_pi.sh and oen can write a group file

--- ssh_pi.sh
+++ ssh_pi.sh
@@ -1,1 +1,19 @@
-for i in 1 3 5 7; do echo "--- This is pi$i ---"; ssh pi@pi$i "$@"; done;
+if [ $1 = '-n' ]    # specify one device
+then
+    group=$2
+    cmd="${@:3}"
+else                # specify a file contains a group
+    cmd="${@:2}"
+    if [ ! -e $1 ]    # if specified a group file name
+    then 
+        echo "There is not Group file named $1!"
+        exit 1
+    else
+        group=$(cat $1)
+    fi
+fi
+
+for i in $group
+    do echo "--- This is pi$i ---" 
+    ssh pi@pi$i "$cmd"
+done
