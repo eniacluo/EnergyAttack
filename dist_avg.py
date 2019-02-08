07cbfd225d8a5f22b501798069c3fc34163d04a1
simon <eniacsimon@gmail.com>
2019-02-07 21:44:55 -0500

    update dist_avg.py - for vm test

--- dist_avg.py
+++ dist_avg.py
@@ -5,23 +5,30 @@ import time
 import numpy as np
 
 UDP_PORT = 5011
-SEND_ADDR = '127.0.0.1'
+BCAST_ADDR = '10.0.0.255'
 BIND_ADDR = '0.0.0.0'
+
+VECTOR_SIZE = 1
     
 def recv_UDP():
     while True:
         try:
             global recv_buffer
+            global recvfrom_addr
             global sock
             global stop_sign
             time.sleep(0.1)
             if stop_sign == 1:
                 break
-            recv_buffer, addr = sock.recvfrom(1024)
-            # print "received message:", data
+            recv_buffer, recvfrom_addr = sock.recvfrom(1024)
         except NameError:
             print 'the sock is broken.'
-            sys.exit(1);
+            break
+        except socket.error as error:
+            if error.errno != socket.errno.EAGAIN:
+                # data not available
+                print 'the sock error.'
+                break
 
 def send_UDP():
     global addr
@@ -29,23 +36,28 @@ def send_UDP():
     global stop_sign
     while True:
         time.sleep(0.5)
-        data = str(np.random.normal(size=3))
+        data = str(np.random.normal(size=VECTOR_SIZE))
         try:
             if stop_sign == 1:
                 break
-            sock.sendto(data, (SEND_ADDR, UDP_PORT))
+            sock.sendto(data, (BCAST_ADDR, UDP_PORT))
         except socket.error:
             print 'send data failed.'
-            sys.exit(2);
+            break
+        except:
+            print 'Unknown error.'
+            break
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.setblocking(0)
     try:
         sock.bind((BIND_ADDR, UDP_PORT))
     except:
         print 'bind failed.'
-        sys.exit(3)
+        sys.exit()
     
     stop_sign = 0 # for controlling the socket threads
     
@@ -58,18 +70,21 @@ if __name__ == '__main__':
     start_time = time.time()
     
     recv_buffer = 'Nothing received'
+    recvfrom_addr = ()
     
     while True:
         time.sleep(0.2)
         if time.time() - start_time > 5:
             stop_sign = 1 # let the socket threads stop
-            recv_thread.join()
-            send_thread.join()
             try:
                 sock.close()
             except socket.error, msg:
                 print "socket close failed."
+            if recv_thread.is_alive:
+                recv_thread.join()
+            if send_thread.is_alive:
+                send_thread.join()
             break
         else:
-            print 'The packet received is: ' + recv_buffer
+            print 'The packet received is: ' + recv_buffer + ' from ' + str(recvfrom_addr)
 
