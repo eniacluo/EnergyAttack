32a49d2ccdff898effa99727e1ac983a186c7a3f
eniacluo <eniacsimon@gmail.com>
2019-02-08 17:05:34 -0500

    add dist_avg algorithm part and CONSTANTS

--- dist_avg.py
+++ dist_avg.py
@@ -3,13 +3,28 @@ import socket
 import sys
 import time
 import numpy as np
+import json
 
-UDP_PORT = 5011
-BCAST_ADDR = '10.0.0.255'
-BIND_ADDR = '0.0.0.0'
+DEBUG_ITERATION_VALUE   = False
+DEBUG_RECV_PACKAGE      = True
+DEBUG_NEIGHBOR_INFO     = True
 
-VECTOR_SIZE = 1
-    
+UDP_PORT        = 5011
+BCAST_ADDR      = '10.0.0.255'
+CONTROL_ADDR    = '10.0.0.254'
+BIND_ADDR       = '0.0.0.0'
+
+VECTOR_SIZE     = 100 
+
+CMD_TERMINATE   = 1
+CMD_START       = 2
+
+# how long does the program runs
+TESTING_TIME_LENGTH = 60
+
+# how many points to print of np array
+np.set_printoptions(threshold = 20)
+
 def recv_UDP():
     while True:
         try:
@@ -17,10 +32,13 @@ def recv_UDP():
             global recvfrom_addr
             global sock
             global stop_sign
-            time.sleep(0.1)
+            global data_ready
             if stop_sign == 1:
                 break
-            recv_buffer, recvfrom_addr = sock.recvfrom(1024)
+            recv_buffer, recvfrom_addr = sock.recvfrom(VECTOR_SIZE * 80)
+            if DEBUG_RECV_PACKAGE:
+                print "Received a package from %s and len = %d." %(str(recvfrom_addr), len(recv_buffer))
+            data_ready = 1
         except NameError:
             print 'the sock is broken.'
             break
@@ -34,19 +52,56 @@ def send_UDP():
     global addr
     global sock
     global stop_sign
+    global iter_index
     while True:
         time.sleep(0.5)
-        data = str(np.random.normal(size=VECTOR_SIZE))
+        value = [float(x) for x in np.random.normal(size=VECTOR_SIZE)]
+        send_data = {'iter': iter_index, 'data': value, 'time': time.time()}
+        json_data = json.dumps(send_data)
         try:
             if stop_sign == 1:
                 break
-            sock.sendto(data, (BCAST_ADDR, UDP_PORT))
-        except socket.error:
-            print 'send data failed.'
+            sock.sendto(json.dumps(send_data), (BCAST_ADDR, UDP_PORT))
+            iter_index = iter_index + 1
+        except socket.error, msg:
+            print 'send data failed.' + str(msg)
             break
-        except:
-            print 'Unknown error.'
+        except Exception as error:
+            print 'Unknown error: ' + str(error)
             break
+
+def dist_avg(data_dict):
+    global neighbor_list
+    global local_value
+    # neighbor_list format: {'10.0.0.1': <vec1>, 
+    #                       '10.0.0.2': <vec2>, ...}
+    neighbor_list[data_dict['addr']] = data_dict['data']
+    all_vectors = np.array([0.0] * VECTOR_SIZE)
+    for vector in neighbor_list.values():
+        all_vectors = all_vectors + np.array(vector)
+    local_value = all_vectors / len(neighbor_list)
+    if DEBUG_ITERATION_VALUE:
+        print '---iter = %d---\nvalue = \n%s' % (data_dict['iter'], (local_value))
+
+def parse_buffer(data_buffer, addr):
+    data_dict = json.loads(data_buffer)
+    if type(data_dict) == dict:
+        try:
+            if data_dict['iter'] == 0:
+                print "iter = %d, cmd received." % data_dict['iter']
+                if addr == CONTROL_ADDR:
+                    return data_dict['cmd']
+                else:
+                    print "received cmd from unknown person and dropped."
+            else:
+                data_dict['addr'] = addr
+                dist_avg(data_dict)
+        except Exception as error:
+            print str(error)
+            print 'an unknown format package from %s and length = %s' % (str(addr), len(str(data_buffer)))
+    else:
+        print 'a broken packege received from %s and length = %s' % (str(addr), len(str(data_buffer)))
+    return 0
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -60,6 +115,8 @@ if __name__ == '__main__':
         sys.exit()
     
     stop_sign = 0 # for controlling the socket threads
+    data_ready = 0
+    iter_index = 0
     
     recv_thread = threading.Thread(target=recv_UDP)
     recv_thread.start()
@@ -71,10 +128,12 @@ if __name__ == '__main__':
     
     recv_buffer = 'Nothing received'
     recvfrom_addr = ()
+
+    neighbor_list = {}
+    local_value = np.array([0.0] * VECTOR_SIZE)
     
     while True:
-        time.sleep(0.2)
-        if time.time() - start_time > 5:
+        if time.time() - start_time > TESTING_TIME_LENGTH:
             stop_sign = 1 # let the socket threads stop
             try:
                 sock.close()
@@ -86,5 +145,11 @@ if __name__ == '__main__':
                 send_thread.join()
             break
         else:
-            print 'The packet received is: ' + recv_buffer + ' from ' + str(recvfrom_addr)
-
+            if data_ready == 1:
+                # print 'The packet received is: ' + recv_buffer + ' from ' + str(recvfrom_addr)
+                cmd = parse_buffer(recv_buffer, str(recvfrom_addr[0]))
+                if cmd == CMD_TERMINATE:
+                    stop_sign = 1
+                else:
+                    pass
+                data_ready = 0
